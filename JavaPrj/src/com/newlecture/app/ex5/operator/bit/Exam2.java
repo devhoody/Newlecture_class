34faff72a259841aeb7efcbf64e136b0d516bd05
Noh <rethonly@gmail.com>
2023-06-12 19:43:18 +0900

    Exam2 - 삼항연산자를 이용하여 2~3자 이름만 출력하기

---
+++ JavaPrj/src/com/newlecture/app/ex5/operator/bit/Exam2.java
@@ -0,0 +1,64 @@
+package com.newlecture.app.ex5.operator.bit;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Exam2 {
+
+	public static void main(String[] args) throws IOException {
+		String name;
+		int length;
+		String result;
+
+		FileInputStream fis = new FileInputStream("res/students2.data");
+		Scanner fscan = new Scanner(fis);
+
+		FileOutputStream fos = new FileOutputStream("res/students2-out.data");
+		PrintWriter fout = new PrintWriter(fos);
+
+		{
+			name = fscan.nextLine();
+			length = name.length();
+			result = (length == 3) ? name : "오류";
+			fout.print(result);
+		}
+
+		{
+			name = fscan.nextLine();
+			length = name.length();
+			result = (length == 3) ? name : "오류";
+			fout.print(result);
+		}
+
+		{
+			name = fscan.nextLine();
+			length = name.length();
+			result = (length == 3) ? name : "오류";
+			fout.print(result);
+		}
+
+		{
+			name = fscan.nextLine();
+			length = name.length();
+			result = (length == 3) ? name : "오류";
+			fout.print(result);
+		}
+		{
+			name = fscan.nextLine();
+			length = name.length();
+			result = (length == 3) ? name : "오류";
+			fout.print(result);
+		}
+
+		fout.close();
+		fos.close();
+		fscan.close();
+		fis.close();
+
+		System.out.println("작업완료");
+
+	}
+}
