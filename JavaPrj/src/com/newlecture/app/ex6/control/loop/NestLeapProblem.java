ca31ab925221b98f49f2ff546306541dd9d88de8
devhoody <rethnoly@gmail.com>
2023-06-16 11:53:50 +0900

    NestLeap 피라미드식출력 문제 3
    add 내가 한 풀이 = 선생님 풀이

--- JavaPrj/src/com/newlecture/app/ex6/control/loop/NestLeapProblem.java
+++ JavaPrj/src/com/newlecture/app/ex6/control/loop/NestLeapProblem.java
@@ -1,18 +1,21 @@
 /*
  * 문제 :1~6 까지 피라미드식으로 출력하기 위해 4번 반복하는 코드를 작성하시오. 
- * 1 2 3
- * 1 2 3 4
- * 1 2 3 4 5 
+ * 1 2 3 4 5 6 7 8 9 10
+ * 1 2 3 4 5 6 7 8 9
+ * 1 2 3 4 5 6 7 8 
+ * 1 2 3 4 5 6 7 
  * 1 2 3 4 5 6
+ * 1 2 3 4 5
+ * 
  *
  * 푼날짜: 230616
  * 내가 푼 방법 : 
  * 		이중 for문 이용.
  * 		index i j 이용.
- *		j < i + 3 
+ *		j < 10 - i 
  * 
  * 선생님 풀이 : 
- * 		j < 3 + i
+ * 		j < 10 - i
  */
 
 package com.newlecture.app.ex6.control.loop;
@@ -20,8 +23,8 @@ package com.newlecture.app.ex6.control.loop;
 public class NestLeapProblem {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 3 + i; j++)
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 10 - i; j++)
 				System.out.printf("%d ", j + 1);
 			System.out.println();
 		}
