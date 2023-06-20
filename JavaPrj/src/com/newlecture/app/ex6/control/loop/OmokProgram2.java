a1a16c7a845944007db6d3320cfd0f30a614b069
devhoody <rethnoly@gmail.com>
2023-06-20 12:19:41 +0900

    OmokProgram(메뉴추가)

---
+++ JavaPrj/src/com/newlecture/app/ex6/control/loop/OmokProgram2.java
@@ -0,0 +1,142 @@
+/*
+ * 바둑게임 
+┌────────────┐
+│      omok game         │
+└────────────┘
+
+1. 게임시작
+2. 도움말
+3. 종료
+ *
+ * 푼날짜: 230620 화 
+ * 내가 푼 방법 : 
+ * 선생님 풀이 :
+ */
+
+package com.newlecture.app.ex6.control.loop;
+
+import java.util.Scanner;
+
+public class OmokProgram2 {
+
+	public static void main(String[] args) {
+		Scanner fscan = new Scanner(System.in);
+		
+		int menu;
+		while(true) {
+			System.out.println("┌────────────┐");
+			System.out.println("│      omok game         │");
+			System.out.println("└────────────┘");
+			System.out.println();
+			System.out.println("1. 게임시작");
+			System.out.println("2. 도움말");
+			System.out.println("3. 종료");
+			System.out.print("> ");
+			System.out.println();
+		
+			menu = fscan.nextInt();
+		
+			// 1.게임시작
+			if (menu == 1) {
+				{
+					// 10 x 10
+					for (int j = 0; j < 12; j++) {
+						for (int i = 0; i < 12; i++)
+							if (i == 0 && j == 0)
+								System.out.print("┌");
+							else if (i == 0 && j == 11)
+								System.out.print("└");
+							else if (i == 11 && j == 0)
+								System.out.print("┐");
+							else if (i == 11 && j == 11)
+								System.out.print("┘");
+							else if (i == 0)
+								System.out.print("├");
+							else if (j == 0)
+								System.out.print("┬");
+							else if (j == 11)
+								System.out.print("┴");
+							else if (i == 11)
+								System.out.print("┤");
+							else
+								System.out.print("┼");
+						System.out.println();
+					}
+					{
+						
+						while (true) {
+							
+							int ox = -1;
+							int oy = -1;
+							
+							Scanner scan = new Scanner(System.in);
+							
+							do {
+								
+								System.out.println("그만두기 : -1 0");
+								System.out.print("x sp y : ");
+								ox = scan.nextInt();
+								if (ox == -1) {
+									System.out.println("프로그램종료");
+									return;
+								}
+								oy = scan.nextInt();
+								
+								if (!((1 <= ox && ox <= 10) && (1 <= oy && oy <= 10))) {
+									System.out.println("오목 좌표의 범위(-1 or 1~10)을 벗어났습니다.");
+								}
+								
+							} while (!((1 <= ox && ox <= 10) && (1 <= oy && oy <= 10)));
+							
+							for (int j = 0; j < 12; j++) {
+								for (int i = 0; i < 12; i++)
+									if (i == 0 && j == 0)
+										System.out.print("┌");
+									else if (i == 0 && j == 11)
+										System.out.print("└");
+									else if (i == 11 && j == 0)
+										System.out.print("┐");
+									else if (i == 11 && j == 11)
+										System.out.print("┘");
+									else if (ox == i && oy == j)
+										System.out.print("ㅁ");
+									else if (i == 0)
+										System.out.print("├");
+									else if (j == 0)
+										System.out.print("┬");
+									else if (j == 11)
+										System.out.print("┴");
+									else if (i == 11)
+										System.out.print("┤");
+									else
+										System.out.print("┼");
+								System.out.println();
+							}
+						}
+					}
+				}
+		
+			}
+			// 2. 도움말
+			else if (menu == 2) {
+				System.out.println("도움말");
+			}
+			// 3. 종료
+			else if (menu == 3) {
+				System.out.println("종료");
+				return;
+			}
+			
+			else {
+				System.out.println("입력 값은 1~3안으로 입력해주세요.");
+			}
+		}
+	} // main off
+
+	public static boolean isNum(int ox, int oy) {
+		boolean isNum1 = false;
+		if ((!(1 <= ox && ox <= 10) || !(1 <= oy && oy <= 10)))
+			isNum1 = true;
+		return isNum1;
+	}
+} // class off
