6ba870fe802398b4ee9189a2053a9d3c229f5adb
devhoody <rethnoly@gmail.com>
2023-06-21 12:02:39 +0900

    array Program
    90을 찾아서 첫번째 값과 바꾸기 (내 풀이)

---
+++ JavaPrj/src/ex06/array/basic/Program.java
@@ -0,0 +1,68 @@
+package ex06.array.basic;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) throws IOException {
+		int[] nums = new int[100];
+		int size = 0;
+//		FileInputStream fis = new FileInputStream("res/array/data.txt");
+//		Scanner scan = new Scanner(fis);
+				
+		// 값 로드하기 
+		{
+			FileInputStream fis = new FileInputStream("res/array/data.txt");
+			Scanner scan = new Scanner(fis);
+			
+			for(int i=0; scan.hasNext(); i++) {
+				nums[i] = scan.nextInt();
+				size ++;
+			}
+			
+			scan.close();
+			fis.close();
+		}
+		
+		// nums 출력하기
+		// 100, 23,... 70,
+		{	
+			for(int i=0; i < size; i++)  {
+				System.out.printf("%d, ", nums[i]);
+			}
+			System.out.println();
+		}
+		// 위치 찾기
+		{
+			for(int i=0; i< size; i++) {
+				if(nums[i] == 90)
+					System.out.printf("90의 인덱스 : %d", i);
+			}
+			
+		}
+		// 값 바꾸기 
+		{
+			nums[0] = 90;
+			nums[5] = 100;
+		}
+		{
+		// 값 저장
+			FileOutputStream fos = new FileOutputStream("res/array/data-out.txt");
+			PrintWriter pw = new PrintWriter(fos);
+			
+			for(int i =0; i<size; i++) {
+				pw.printf("%d ", nums[i]);
+			}
+			
+			pw.flush();
+			
+			pw.close();
+			fos.close();
+		}
+	}
+
+}
