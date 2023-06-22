1c366c61ce11df10aaacd5ac82b000e48498c97c
devhoody <rethnoly@gmail.com>
2023-06-22 12:54:34 +0900

    array Program (버블 정렬 - 오름차순)
    230622
    이중 for문을 이용한 버블 정렬
    내가 한 풀이

--- JavaPrj/src/ex06/array/basic/Program.java
+++ JavaPrj/src/ex06/array/basic/Program.java
@@ -88,6 +88,7 @@ public class Program {
 			System.out.printf("max : %d\n", nums[size - 1]);
 		}
 		System.out.println("--------------------------------------");
+		
 		// 출력 
 		{
 			for (int i = 0; i < size; i++) 
@@ -106,16 +107,39 @@ public class Program {
 			System.out.printf("min index : %d\n", minIndex);
 		}
 		
-		//자리바꾸기 방식으로 최소값 찾기 
+		System.out.println("--------------------------------------");
+
+		// 출력 
 		{
-			int temp= 0;
-			for (int i = 0; i < size - 1; i++) {
-				
-				if (nums[i] < nums[i + 1])
-					nums[i+1] = nums[i];
-			}
-			System.out.printf("min : %d\n", nums[size - 1]);
+			for (int i = 0; i < size; i++) 
+				System.out.printf("%d, ", nums[i]);
+			
+			System.out.println();
 		}
+		
+		// 버블 정렬
+		{
+			for(int j =size-1; j>0; j--) {
+				for(int i =0; i<j; i++) {
+					if (nums[i] > nums[i + 1]) {
+						int temp = nums[i+1]; // 선언을 최소한으로 줄이기 위해 if 문 안으로 넣음.
+						nums[i + 1] = nums[i];
+						nums[i] = temp;
+					}
+				}
+			}
+		}
+		
+		//자리바꾸기 방식으로 최소값 찾기 
+//		{
+//			int temp= 0;
+//			for (int i = 0; i < size - 1; i++) {
+//				
+//				if (nums[i] < nums[i + 1])
+//					nums[i+1] = nums[i];
+//			}
+//			System.out.printf("min : %d\n", nums[size - 1]);
+//		}
 
 		// data.txt에 값 저장
 		{
