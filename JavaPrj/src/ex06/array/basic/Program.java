abe5babfed4b15ed7d2f4b6b2ec63f7f2ea59c45
devhoody <rethnoly@gmail.com>
2023-06-23 10:51:42 +0900

    Array Program(선택정렬)
    230623
    내가 한 풀이

--- JavaPrj/src/ex06/array/basic/Program.java
+++ JavaPrj/src/ex06/array/basic/Program.java
@@ -29,9 +29,9 @@ public class Program {
 
 		// nums 출력하기
 		{
-			for (int i = 0; i < size; i++) 
+			for (int i = 0; i < size; i++)
 				System.out.printf("%d, ", nums[i]);
-			
+
 			System.out.println();
 		}
 		// 위치 찾기
@@ -48,18 +48,18 @@ public class Program {
 		}
 		// 값 바꾸기
 
-		{
-			int temp; // 선언
-			temp = nums[0]; // nums[0] -> num_
-			nums[0] = nums[index]; // nums[index] -> num[0]
-			nums[index] = temp; // num_ -> nums[index]
-
-		}
+//		{
+//			int temp; // 선언
+//			temp = nums[0]; // nums[0] -> num_
+//			nums[0] = nums[index]; // nums[index] -> num[0]
+//			nums[index] = temp; // num_ -> nums[index]
+//
+//		}
 		// index[0]<->90 nums 출력하기
 		{
-			for (int i = 0; i < size; i++) 
+			for (int i = 0; i < size; i++)
 				System.out.printf("%d, ", nums[i]);
-			
+
 			System.out.println();
 		}
 
@@ -76,61 +76,90 @@ public class Program {
 		}
 
 		// 자리바꾸기 방식으로 큰 값을 찾기
-		{
-			for (int i = 0; i < size - 1; i++) {
-				
-				if (nums[i] > nums[i + 1]) {
-					int temp = nums[i+1]; // 선언을 최소한으로 줄이기 위해 if 문 안으로 넣음.
-					nums[i + 1] = nums[i];
-					nums[i] = temp;
-				}
-			}
-			System.out.printf("max : %d\n", nums[size - 1]);
-		}
+//		{
+//			for (int i = 0; i < size - 1; i++) {
+//				
+//				if (nums[i] > nums[i + 1]) {
+//					int temp = nums[i+1]; // 선언을 최소한으로 줄이기 위해 if 문 안으로 넣음.
+//					nums[i + 1] = nums[i];
+//					nums[i] = temp;
+//				}
+//			}
+//			System.out.printf("max : %d\n", nums[size - 1]);
+//		}
 		System.out.println("--------------------------------------");
-		
-		// 출력 
+
+		// 출력
 		{
-			for (int i = 0; i < size; i++) 
+			for (int i = 0; i < size; i++)
 				System.out.printf("%d, ", nums[i]);
-			
+
 			System.out.println();
 		}
-		
-		// 최솟값의 인덱스 찾기 
+
+		// 최솟값의 인덱스 찾기
 		{
 			int minIndex = 0;
-			for (int i=0; i< size -1; i++) {
-				if(nums[minIndex]>nums[i+1]) 
-					minIndex = i+1;
+			for (int i = 0; i < size - 1; i++) {
+				if (nums[minIndex] > nums[i + 1])
+					minIndex = i + 1;
 			}
 			System.out.printf("min index : %d\n", minIndex);
 		}
-		
+
 		System.out.println("--------------------------------------");
 
-		// 출력 
+		// 출력
 		{
-			for (int i = 0; i < size; i++) 
+			for (int i = 0; i < size; i++)
 				System.out.printf("%d, ", nums[i]);
-			
+
 			System.out.println();
 		}
-		
+
 		// 버블 정렬
+//		{
+//			for(int j =0; j<size-1; j++) 
+//				for(int i =0; i<size-1-j; i++) 
+//					if (nums[i] > nums[i + 1]) {
+//						int temp = nums[i+1]; // 선언을 최소한으로 줄이기 위해 if 문 안으로 넣음.
+//						nums[i + 1] = nums[i];
+//						nums[i] = temp;
+//					}
+//		}
+
+		// 선택 정렬
 		{
-			for(int j =0; j<size-1; j--) {
-				for(int i =0; i<size-1-j; i++) {
-					if (nums[i] > nums[i + 1]) {
-						int temp = nums[i+1]; // 선언을 최소한으로 줄이기 위해 if 문 안으로 넣음.
-						nums[i + 1] = nums[i];
-						nums[i] = temp;
-					}
-				}
+			int minIndex = 0;
+//			for(int j=0, minIndex = 0 ; j<size -1 && minIndex < size -1; j++, minIndex++) {
+			for (int j = 0; j < size - 1; j++) {
+				minIndex++;
+				for (int i = 0; i < size - 1 - j; i++)
+					if (nums[minIndex] > nums[i + j])
+						minIndex = i + j;
+				int temp = nums[j];
+				nums[j] = nums[minIndex];
+				nums[minIndex] = temp;
 			}
+
+//			System.out.printf("min index : %d\n", minIndex);
 		}
-		
-		//자리바꾸기 방식으로 최소값 찾기 
+		{
+			int j = 1;
+			int minIndex = 1;
+
+			for (int i = 0; i < size - 2; i++)
+				if (nums[minIndex] > nums[i + 2])
+					minIndex = i + 2;
+
+			int temp = nums[j];
+			nums[j] = nums[minIndex];
+			nums[minIndex] = temp;
+
+			System.out.printf("min index : %d\n", minIndex);
+		}
+
+		// 자리바꾸기 방식으로 최소값 찾기
 //		{
 //			int temp= 0;
 //			for (int i = 0; i < size - 1; i++) {
