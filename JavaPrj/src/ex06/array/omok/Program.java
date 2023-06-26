62d8a4d6c41fa273bf879ba4abbc61020ef1dd84
devhoody <rethnoly@gmail.com>
2023-06-26 10:49:19 +0900

    230626 오목게임 (width, height 추가)
    내가 한 풀이
    : int width, height 추가

--- JavaPrj/src/ex06/array/omok/Program.java
+++ JavaPrj/src/ex06/array/omok/Program.java
@@ -26,45 +26,39 @@ public class Program {
 
 		int ox = 0;
 		int oy = 0;
-
-		char[][] board = new char[12][12];
-
-		for (int y = 0; y < 12; y++) {
-			for (int x = 0; x < 12; x++) {
+		
+		int width = 12;
+		int height = 12;
+		
+		char[][] board = new char[height][width];
+		
+		for (int y = 0; y < height; y++) 
+			for (int x = 0; x < width; x++) {
 				board[y][x] = '┼';
-				if (y == 0 && x ==0)
-					board[y][x] = '┌';
-				else if (y == 0 && x == 11)
-					board[y][x] = '┐';
-				else if (y == 0 )
-					board[y][x] = '┬';
-				else if (y == 11 && x == 0)
-					board[y][x] = '└';
-				else if (y == 11 && x == 11)
-					board[y][x] = '┘';
-				else if (x == 0)
-					board[y][x] = '├';
-				else if (x == 11)
-					board[y][x] = '┤';
-				else if (y == 11)
-					board[y][x] = '┴';
+				board[0][x] = '┬';
+				board[height-1][x] = '┴';
+				board[y][0] = '├';
+				board[y][width-1] = '┤';
+				board[0][0] = '┌';
+				board[0][width-1] = '┐';
+				board[height-1][0] = '└';
+				board[height-1][width-1] = '┘';
 			}
-		}
-		boolean onDark = true;
 		
 		while (true) {
 			// 흑돌 입력
+			boolean onDark = true;
+			
 			do {
-				System.out.println("흑돌 > ");
+				System.out.println("흑돌(범위 : 0~11) > ");
 				oy = scan.nextInt();
 				ox = scan.nextInt();
 
-				if (board[oy][ox] == '백') {
-					System.out.println("오목알의 위치가 겹칩니다.");
-				}
-				if ((oy < 0 || 11 < oy) || (ox < 0 || 11 < ox)) {
+				if (board[oy][ox] == '백') 
+					System.out.println("오목알의 위치가 겹칩니다. 다시 입력해주세요.");
+				
+				if ((oy < 0 || 11 < oy) || (ox < 0 || 11 < ox)) 
 					System.out.println("값의 범위는 0~11사이로 입력바랍니다.");
-				}
 				
 				onDark = (board[oy][ox] == '백') || (oy < 0 || 11 < oy) || (ox < 0 || 11 < ox);
 			} while (onDark);
@@ -72,8 +66,8 @@ public class Program {
 			board[oy][ox] = '흑';
 
 			// 흑돌 출력
-			for (int y = 0; y < 12; y++) {
-				for (int x = 0; x < 12; x++)
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++)
 					System.out.printf("%c", board[y][x]);// [j][i]
 				System.out.println();
 			}
@@ -81,26 +75,24 @@ public class Program {
 			
 			// 백돌 입력
 			do {
-				System.out.println("백돌 > ");
+				System.out.println("백돌(범위 : 0~11) > ");
 				oy = scan.nextInt();
 				ox = scan.nextInt();
 
-				if (board[oy][ox] == '흑') {
+				if (board[oy][ox] == '흑') 
 					System.out.println("오목알의 위치가 겹칩니다.");
-				}
 
-				if ((oy < 0 || 11 < oy) || (ox < 0 || 11 < ox)) {
+				if ((oy < 0 || 11 < oy) || (ox < 0 || 11 < ox)) 
 					System.out.println("값의 범위는 0~11사이로 입력바랍니다.");
-				}
-				
+
 				onWhite = (board[oy][ox] == '흑') || (oy < 0 || 11 < oy) || (ox < 0 || 11 < ox);
 			} while (onWhite);
 
 			board[oy][ox] = '백';
 
 			// 백돌 출력
-			for (int y = 0; y < 12; y++) {
-				for (int x = 0; x < 12; x++)
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++)
 					System.out.printf("%c", board[y][x]);// [j][i]
 				System.out.println();
 			}
