5271e81df0108ffa6657f2c3f8534b447254e4e4
devhoody <rethnoly@gmail.com>
2023-06-28 14:18:38 +0900

    230625 이차배열 이용

--- JavaPrj/src/ex07/zarray/Program.java
+++ JavaPrj/src/ex07/zarray/Program.java
@@ -5,10 +5,10 @@ public class Program {
 	public static void main(String[] args) {
 		int[][] lottos = new int[][] { { 1, 2, 3, 4, 5, 6 }, { 7, 8, 9, 10, 11, 12 }, { 13, 14, 15, 16, 17, 18 } };
 
-		for (int j = 0; j < 3; j++) {
+		for (int j = 0; j < 3; j++)
 			for (int i = 0; i < 6; i++)
 				System.out.printf("%d,", lottos[j][i]);
-		}
+
 		System.out.println();
 
 		// [0] <-> [2]
@@ -19,10 +19,9 @@ public class Program {
 
 		// lottos[1] = new int[] { 9, 9, 99, 99, 9, 9, 9, 9, 1 }; // 초기화x , 새로운 배열을 만들때.
 
-		for (int j = 0; j < 3; j++) {
+		for (int j = 0; j < 3; j++)
 			for (int i = 0; i < 6; i++)
 				System.out.printf("%d,", lottos[j][i]);
-		}
 
 		System.out.println();
 
