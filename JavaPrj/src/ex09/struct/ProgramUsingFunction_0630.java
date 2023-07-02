10b999b98f80932e6f731da7d23faaffddd86c1f
devhoody <rethnoly@gmail.com>
2023-07-02 20:29:11 +0900

    230702 멘토링 성적출력 (함수, 클래스 이용)

---
+++ JavaPrj/src/ex09/struct/ProgramUsingFunction_0630.java
@@ -0,0 +1,109 @@
+package ex09.struct;
+// Exams 객체 배열과 Exam객체와의 관계 
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ProgramUsingFunction_0630 {
+
+	public static void main(String[] args) throws IOException {
+		ExamList list = new ExamList();
+		
+	
+		
+		
+		
+		// 성적 로드
+		{
+			road(list);
+		}
+		
+		// 성적 정렬 
+		{
+			sort(list);
+		}
+		
+		// 성적 출력
+		{
+			print(list);
+		}
+	}
+
+	private static void print(ExamList list) {
+		Exam[] exams = list.exams;
+		int size = list.index;
+		// exam[] -> exam 객체 변환 후 출력 .
+		for (int i = 0; i < size; i++) {
+			Exam exam = exams[i];
+			int kor = exam.kor;
+			int eng = exam.eng;
+			int math = exam.math;
+			
+			int total = kor + eng + math;
+			float avg = total / 3.0f;
+			
+			System.out.printf("%d등 : %d \t %d \t %d \t %d \t %.2f \n", i+1, kor, eng, math, total, avg);
+		}
+		
+	}
+
+	private static void sort(ExamList list) {
+		Exam[] exams = list.exams;
+		int size = list.index;
+
+		for (int i = 0; i < size - 1; i++) {
+			for (int j = 0; j < size - 1 - i; j++) {
+				if (exams[j].total < exams[j + 1].total) { // 작은 값을 뒤로 보내기(내림차순)
+					Exam temp = exams[j];
+					exams[j] = exams[j + 1];
+					exams[j + 1] = temp;
+				}
+			}
+		}
+		
+	}
+
+	private static void road(ExamList list) throws IOException {
+		
+		Exam[] exams = new Exam[20]; // exams[0],exams[1]... exams[19]까지의 20개 이름표생성
+		int index = 0;
+
+		{
+			FileInputStream fis = new FileInputStream("res/ex09/exam.data");
+			Scanner fscan = new Scanner(fis);
+			
+			list.exams = new Exam[20];
+			list.index = 0;
+			
+			fscan.nextLine(); // 맨 윗줄날리기
+
+			while (fscan.hasNextLine()) // 마지막 줄까지 읽어오기
+			{
+				Exam exam = new Exam(); // Exam exam : 한번 만들어짐. while문 들어오기 전에 한번 생성
+													// /new Exam() : while 문 돌때마다 객체(공간)을 만들어 준다.
+
+				String line = fscan.nextLine();
+				String[] tokens = line.split(",");
+
+				exam.kor = Integer.parseInt(tokens[0]);
+				exam.eng = Integer.parseInt(tokens[1]);
+				exam.math = Integer.parseInt(tokens[2]);
+				exam.total = exam.kor + exam.eng + exam.math;
+				exam.avg = exam.total / 3.0f;
+
+				exams[index] = exam; // 만든 공간에 exams[index]의 이름표 붙여준다. 참조 변수를 exams[]로대입
+				index++;
+			}
+
+			fscan.close();
+			fis.close();
+
+			list.exams = exams;
+			list.index = index;
+	} // main off
+
+	}
+
+} // class off
