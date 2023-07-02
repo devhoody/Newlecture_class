62fecca64f58d2b26500800f77928f21bf76ccdb
devhoody <rethnoly@gmail.com>
2023-07-02 20:27:51 +0900

    230702 total, avg 추가

--- JavaPrj/src/ex09/struct/Program_0629.java
+++ JavaPrj/src/ex09/struct/Program_0629.java
@@ -2,6 +2,7 @@ package ex09.struct;
 // Exams 객체 배열과 Exam객체와의 관계 
 
 // 230629
+// 230630
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class Program_0629 {
 
 		Exam[] exams = new Exam[20]; // exams[0],exams[1]... exams[19]까지의 20개 이름표생성
 		int index = 0;
+
 		{
 			FileInputStream fis = new FileInputStream("res/ex09/exam.data");
 			Scanner fscan = new Scanner(fis);
@@ -22,7 +24,7 @@ public class Program_0629 {
 			while (fscan.hasNextLine()) // 마지막 줄까지 읽어오기
 			{
 				Exam exam = new Exam(); // Exam exam : 한번 만들어짐. while문 들어오기 전에 한번 생성
-													// new Exam() : while 문 돌때마다 객체(공간)을 만들어 준다.
+										// new Exam() : while 문 돌때마다 객체(공간)을 만들어 준다.
 
 				String line = fscan.nextLine();
 				String[] tokens = line.split(",");
@@ -30,6 +32,8 @@ public class Program_0629 {
 				exam.kor = Integer.parseInt(tokens[0]);
 				exam.eng = Integer.parseInt(tokens[1]);
 				exam.math = Integer.parseInt(tokens[2]);
+				exam.total = exam.kor + exam.eng + exam.math;
+				exam.avg = exam.total / 3.0f;
 
 				exams[index] = exam; // 만든 공간에 exams[index]의 이름표 붙여준다. 참조 변수를 exams[]로대입
 				index++;
@@ -41,17 +45,26 @@ public class Program_0629 {
 		}
 		// 출력
 		{
+			// 첫 번째 학생 성적이 두 번째 학생의 성적보다 크다면 바꿔서 출력 .
+			if (exams[0].total > exams[1].total) {
+				Exam temp = exams[0];
+				exams[0] = exams[1];
+				exams[1] = temp;
+			}
+
 			int size = index;
 
-			// exam, kor ,eng, math변수를 선언하여 print문의 가시성을 높인다.
+			// exam[] -> exam 객체 변환 후 출력 .
 			for (int i = 0; i < size; i++) {
 				Exam exam = exams[i];
-
 				int kor = exam.kor;
 				int eng = exam.eng;
 				int math = exam.math;
 
-				System.out.printf("%d %d %d \n", kor, eng, math);
+				int total = kor + eng + math;
+				float avg = total / 3.0f;
+
+				System.out.printf("%d \t %d \t %d \t %d \t %.2f \n", kor, eng, math, total, avg);
 			}
 
 		}
