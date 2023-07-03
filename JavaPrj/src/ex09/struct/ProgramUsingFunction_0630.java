043ce507747c043bd7b6fc3cce29ed27e521f310
devhoody <rethnoly@gmail.com>
2023-07-03 10:58:30 +0900

    230703 성적출력 (Sn-1 + An 이용)

--- JavaPrj/src/ex09/struct/ProgramUsingFunction_0630.java
+++ JavaPrj/src/ex09/struct/ProgramUsingFunction_0630.java
@@ -33,10 +33,10 @@ public class ProgramUsingFunction_0630 {
 
 	private static int sum(int n) {
 
-		if (n == 0)
-			return 0;
-
-		return n += sum(n - 1);
+		if(n==1)
+			return 1;
+		
+		return  sum(n-1) + n;
 	}
 
 	private static void print(ExamList list, int limit) {
