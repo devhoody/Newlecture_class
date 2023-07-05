ab4d1031687c3bf2b2934d057a3b795176fdad67
devhoody <rethnoly@gmail.com>
2023-07-05 11:51:10 +0900

    230705 total, avg, save, load 추가

--- JavaPrj/src/ex11/oop/basic/Program.java
+++ JavaPrj/src/ex11/oop/basic/Program.java
@@ -1,19 +1,24 @@
 package ex11.oop.basic;
 
+import java.io.IOException;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		Exam exam = new Exam();
-		Exam exam1 = new Exam();
-		Exam exam2 = new Exam();
 
+		exam.input();
+		
+		int total = exam.total();
+		float avg = exam.avg();
+		exam.save("exam.data");
+		exam.load("exam.data");
+		
 		exam.print();
-
-//		Exam.input(exam);
-
-//		exam.input();
-//		
+		
+		
+		
 //		System.out.println(exam.kor);
 	}
 
