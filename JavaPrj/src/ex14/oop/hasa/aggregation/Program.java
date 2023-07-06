81aabc29cd28930090c02e4a453356e9bb2cf10a
devhoody <rethnoly@gmail.com>
2023-07-06 14:20:15 +0900

    230706 Has a (Aggregation)

---
+++ JavaPrj/src/ex14/oop/hasa/aggregation/Program.java
@@ -0,0 +1,16 @@
+package ex14.oop.hasa.aggregation;
+
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) throws IOException {
+		ExamConsole console = new ExamConsole();
+		
+		console.input();
+//		console.print(2);
+		console.printAll();
+		
+	}
+
+}
