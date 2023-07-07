c9944e45c4e27b04dffd8854bc08bfc7485c7d1e
devhoody <rethnoly@gmail.com>
2023-07-07 20:42:52 +0900

    230705 loadmap add function

--- Mentoring/src/com/NewlecMentoring/Quiz_0703_2.java
+++ Mentoring/src/com/NewlecMentoring/Quiz_0703_2.java
@@ -1,6 +1,7 @@
 package com.NewlecMentoring;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -13,28 +14,8 @@ public class Quiz_0703_2 {
 
 		// 3. res/map.txt에서 읽은 데이터로 map 배열을 채우시오.
 
-		{
-			// 코드를 작성하는 공간
-			FileInputStream fis = new FileInputStream("res/map.txt");
-			Scanner fscan = new Scanner(fis);
-
-			for (int i = 0; i < 3; i++) {
-				String str = fscan.nextLine();
-				String[] str2 = str.split("");
-
-				for (int j = 0; j < 5; j++) {
-					map[i][j] = Integer.parseInt(str2[j]);
-				}
-			}
-
-			for (int i = 0; i < 3; i++) {
-				for (int j = 0; j < 5; j++)
-					System.out.printf("%d", map[i][j]);
-				System.out.println();
-			}
-
-			System.out.println("map 데이터 로드 완료");
-		}
+		map = loadMap(map);
+			
 		// 4. map 데이터 하나는 board 배열의 4칸과 대응되며 다음과 같은 모양으로 대응된다.
 		// map에서 0은 다음 모양과 같다.
 		// ┌ ┐
@@ -50,7 +31,11 @@ public class Quiz_0703_2 {
 		// └ ┘▩▩└ ┘▩▩└ ┘
 		// ┌ ┐┌ ┐┌ ┐┌ ┐┌ ┐
 		// └ ┘└ ┘└ ┘└ ┘└ ┘
+		
+		printBorad(map, board);
+	}
 
+	private static void printBorad(int[][] map, char[][] board) {
 		for (int x = 0; x < 3; x++) {
 			for (int y = 0; y < 5; y++) {
 				if (map[x][y] == 0) {
@@ -73,7 +58,31 @@ public class Quiz_0703_2 {
 				System.out.printf("%c", board[x][y]);
 			System.out.println();
 		}
+		
+	}
 
+	private static int[][] loadMap(int[][] map) throws IOException {
+		FileInputStream fis = new FileInputStream("res/map.txt");
+		Scanner fscan = new Scanner(fis);
+
+		for (int i = 0; i < 3; i++) {
+			String str = fscan.nextLine();
+			String[] str2 = str.split("");
+
+			for (int j = 0; j < 5; j++) {
+				map[i][j] = Integer.parseInt(str2[j]);
+			}
+		}
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 5; j++)
+				System.out.printf("%d", map[i][j]);
+			System.out.println();
+		}
+
+		System.out.println("map 데이터 로드 완료");
+		
+		return map;
 	}
 
 }
