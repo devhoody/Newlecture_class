35ac9f1106050947b1c2182c42732bf7f8764d38
devhoody <rethnoly@gmail.com>
2023-07-07 20:43:36 +0900

    주석추가

--- JavaPrj/src/ex15/oop/isa/GameFrame.java
+++ JavaPrj/src/ex15/oop/isa/GameFrame.java
@@ -1,16 +1,12 @@
 package ex15.oop.isa;
-/*
- * 230707
- * Frame을 이용한 상속 연습
- */
 
 import java.awt.Frame;
 import java.awt.Graphics;
 
 public class GameFrame extends Frame {
-	@Override
-	public void paint(Graphics g) {
-//		super.paint(g);
-		g.drawLine(0, 0, 300, 300);
-	}
+		@Override
+		public void paint(Graphics g) {
+			// TODO Auto-generated method stub
+			super.paint(g);
+		}
 }
