00fd5de354344105c6271e50ff121f2936cf4a08
devhoody <rethnoly@gmail.com>
2023-07-10 10:08:38 +0900

    230710 추상화

---
+++ JavaPrj/src/ex16/oop/abst/NewExam.java
@@ -0,0 +1,33 @@
+package ex16.oop.abst;
+/*
+ *  0706 
+ *	 is a 상속 NewExam -> Exam
+ */
+
+public class NewExam extends Exam{
+	private int com;
+
+	public NewExam() {
+		this(0,0,0,0);
+	}
+	
+	public NewExam(int kor, int eng, int math, int com) {
+		super(kor, eng, math);
+		this.com = com;
+	}
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+	
+	@Override
+	public int total() {
+
+		return super.total()+com;
+	}
+	
+}
---
+++ JavaPrj/src/ex16/oop/abst/Program.java
@@ -0,0 +1,15 @@
+package ex16.oop.abst;
+/*
+ * 230710
+ * 추상화
+ */
+
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) throws IOException {
+		
+	}
+
+}
