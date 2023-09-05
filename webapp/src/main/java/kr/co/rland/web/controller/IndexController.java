d8b196ac099fd0d9da76ba26eaef58803fa9a886
devhoody <rethnoly@gmail.com>
2023-09-05 12:29:14 +0900

    init : Servlet(feat. Annotation(@WebServlet)

---
+++ webapp/src/main/java/kr/co/rland/web/controller/IndexController.java
@@ -0,0 +1,22 @@
+package kr.co.rland.web.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/index")
+public class IndexController extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("안녕하세요.123123");
+	}
+}
