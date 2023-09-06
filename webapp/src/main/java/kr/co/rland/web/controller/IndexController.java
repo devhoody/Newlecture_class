9c68beee9b662dbfc1b303ffacff24fb46afece4
devhoody <rethnoly@gmail.com>
2023-09-06 11:22:07 +0900

    feat : add type, button
    버튼추가 (객관식, 주관식)

--- webapp/src/main/java/kr/co/rland/web/controller/IndexController.java
+++ webapp/src/main/java/kr/co/rland/web/controller/IndexController.java
@@ -18,16 +18,20 @@ public class IndexController extends HttpServlet {
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
-		
+
 		int size = 4;
 		String size_ = request.getParameter("s");
 		if (size_ != null)
 			size = Integer.parseInt(size_);
 
-		for (int i = 0; i < size; i++) {
+		int type = 1;
+		String type_ = request.getParameter("t");
+		if (type_ != null)
+			type = Integer.parseInt(type_);
+
+		for (int i = 0; i < size; i++)
 			out.println("안녕<br>");
-		}
-		
+
 		out.write("<!DOCTYPE html>");
 		out.write("<html>");
 		out.write("<head>");
@@ -36,7 +40,7 @@ public class IndexController extends HttpServlet {
 		out.write("</head>");
 		out.write("<body>");
 		out.write("Welcome 4 한글");
-		{
+		if (type == 1) {
 			out.write("<hr>");
 			out.write("<h2>인삿말을 몇 번 듣고 싶으세요?</h2>");
 			out.write("<a href=\"/index?s=1\">1번</a><br>");
@@ -44,15 +48,18 @@ public class IndexController extends HttpServlet {
 			out.write("<a href=\"/index?s=10\">10번</a><br>");
 			out.write("<a href=\"/index?s=100\">100번</a>");
 			out.write("<hr>");
-		}
-		{
+		} else if (type == 2) {
 			out.write("<h2>인삿말을 몇 번 듣고 싶으세요?</h2>");
 			out.write("<form action=\"index\">");
 			out.write("	<label>갯수입력</label><input type=\"text\" name=\"s\">");
+			out.write("	<input type=\"hidden\" name=\"t\" value=\"2\">");
 			out.write("	<input type=\"submit\" value=\"요청\">");
 			out.write("</form>");
-			out.write("<hr>");
 		}
+		out.write("<hr>");
+		out.write("<a href=\"/index?t=1\">객관식</a><br>");
+		out.write("<a href=\"/index?t=2\">주관식</a><br>");
+
 		out.write("</body>");
 		out.write("</html>");
 
