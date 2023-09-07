e7dc979b20b68f127d3f718ca30d4c01aaff0479
devhoody <rethnoly@gmail.com>
2023-09-07 13:00:24 +0900

    feat : Servlet Context
    Listener에서 ServeletContext에 SqlSessionFactory 넣기
    - event.getServletContext().setAttribute 이용

--- webapp/src/main/java/kr/co/rland/web/config/AppContextListener.java
+++ webapp/src/main/java/kr/co/rland/web/config/AppContextListener.java
@@ -1,18 +1,39 @@
 package kr.co.rland.web.config;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 
 @WebListener
 public class AppContextListener implements ServletContextListener {
-	@Override
-	public void contextInitialized(ServletContextEvent sce) {
-		System.out.println("서버 시작 이벤트 발생");
-	}
-	
-	@Override
-	public void contextDestroyed(ServletContextEvent sce) {
-		System.out.println("서버 종료 이벤트 발생");
-	}
+
+
+  @Override
+  public void contextInitialized(ServletContextEvent event) {
+    System.out.println("서버 시작 이벤트 발생");
+
+    String resource = "mybatis-config.xml";
+    InputStream inputStream;
+    try {
+      inputStream = Resources.getResourceAsStream(resource);
+      SqlSessionFactory sqlSessionFactory =
+          new SqlSessionFactoryBuilder().build(inputStream);
+      event.getServletContext().setAttribute("SqlSessionFactory", sqlSessionFactory);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent sce) {
+    System.out.println("서버 종료 이벤트 발생");
+  }
 }
