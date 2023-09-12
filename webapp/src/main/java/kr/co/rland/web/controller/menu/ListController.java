02cc863807b3ec7dfa6755f44cdcbe2201a44d3c
devhoody <rethnoly@gmail.com>
2023-09-12 12:42:53 +0900

    feat: update MVCver2(SpringMVC, POJO Controller)
    DetailController.java, ListController.java
    - POJO Controller
    
    app-servlet.xml
    - Spring MVC를 이용한 url 연결
    
    web.xml
    - 프론트 컨트롤러가 / 이후 모든 url을 받기 위해 <url pattern>을 "/" 로 설정
    - Annotation등의 기타 설정을 허용하기 위해서 metadata-complete를 false로 변경

--- webapp/src/main/java/kr/co/rland/web/controller/menu/ListController.java
+++ webapp/src/main/java/kr/co/rland/web/controller/menu/ListController.java
@@ -1,34 +1,49 @@
 package kr.co.rland.web.controller.menu;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.co.rland.web.entity.Menu;
 import kr.co.rland.web.repository.MenuRepository;
 
-@WebServlet("/menu/list")
-public class ListController extends HttpServlet {
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+public class ListController implements Controller {
 
-		ServletContext application = req.getServletContext();
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ServletContext application = request.getServletContext();
 		SqlSessionFactory sqlSessionFactory = (SqlSessionFactory) application.getAttribute("SqlSessionFactory");
 
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 		MenuRepository repository = sqlSession.getMapper(MenuRepository.class);
 		List<Menu> list = repository.findAll();
 
-		req.setAttribute("list", list);
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/WEB-INF/view/menu/list.jsp");
+		mv.addObject("list", list);
 
-		req.getRequestDispatcher("/WEB-INF/view/menu/list.jsp").forward(req, resp);
+		// forwarding
+		return mv;
 	}
+
+//	@Override
+//	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//
+//		ServletContext application = req.getServletContext();
+//		SqlSessionFactory sqlSessionFactory = (SqlSessionFactory) application.getAttribute("SqlSessionFactory");
+//
+//		SqlSession sqlSession = sqlSessionFactory.openSession();
+//		MenuRepository repository = sqlSession.getMapper(MenuRepository.class);
+//		List<Menu> list = repository.findAll();
+//
+//		req.setAttribute("list", list);
+//
+//		req.getRequestDispatcher("/WEB-INF/view/menu/list.jsp").forward(req, resp);
+//	}
 }
