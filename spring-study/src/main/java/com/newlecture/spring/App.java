5f083cf97fa1ea0b09a3890a7382dcf272a5dfaa
devhoody <rethnoly@gmail.com>
2023-09-13 12:39:30 +0900

    feat: DI(Dependency Injection)
    MenuDao dao = new Dm2MenuDao()
    service.setDao(dao)

--- spring-study/src/main/java/com/newlecture/spring/App.java
+++ spring-study/src/main/java/com/newlecture/spring/App.java
@@ -1,14 +1,24 @@
 package com.newlecture.spring;
+/*
+ * 230913
+ * DI
+ */
 
 import java.util.List;
 
+import com.newlecture.spring.dao.MenuDao;
+import com.newlecture.spring.dao.dm.Dm2MenuDao;
 import com.newlecture.spring.entity.Menu;
 import com.newlecture.spring.service.MenuServiceImp;
 
 public class App {
 
 	public static void main(String[] args) {
+		MenuDao dao = new Dm2MenuDao(); // Dependency - 종속객체 -> 부품
+		
 		MenuServiceImp service = new MenuServiceImp();
+		service.setDao(dao); // Injection - 주입 -> 결합 
+		
 		List<Menu> list = service.getList();
 		
 		System.out.println(list);
