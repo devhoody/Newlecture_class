cee6368197c9052c15f88ed6b5c2eb5bc1068078
devhoody <rethnoly@gmail.com>
2023-09-13 12:26:10 +0900

    feat: 인터페이스를 이용한 Dao와 Service 결합력 낮추기
    private DmMenuDao -> MenuDao
    
    public void setDao(MenuDao dao) {
        this.dao = dao;
      }

--- spring-study/src/main/java/com/newlecture/spring/service/MenuServiceImp.java
+++ spring-study/src/main/java/com/newlecture/spring/service/MenuServiceImp.java
@@ -2,20 +2,24 @@ package com.newlecture.spring.service;
 
 import java.util.List;
 
-import com.newlecture.spring.dao.dm.DmMenuDao;
+import com.newlecture.spring.dao.MenuDao;
 import com.newlecture.spring.entity.Menu;
 
-public class MenuServiceImp implements MenuService{
-	
-	private DmMenuDao dao;
-	
-	public MenuServiceImp() {
-		dao = new DmMenuDao();
-	}
-	
-	@Override
-	public List<Menu> getList() {
-		// TODO Auto-generated method stub
-		return dao.findAll();
-	}
+public class MenuServiceImp implements MenuService {
+
+  private MenuDao dao; // 인터페이스를 쓰는 이유??
+
+  public MenuServiceImp() {
+//		dao = new DmMenuDao();
+  }
+
+  public void setDao(MenuDao dao) {
+    this.dao = dao;
+  }
+
+  @Override
+  public List<Menu> getList() {
+    // TODO Auto-generated method stub
+    return dao.findAll();
+  }
 }
