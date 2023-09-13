88bbd9a94b7574537d46849255682f37406299e9
devhoody <rethnoly@gmail.com>
2023-09-13 10:03:07 +0900

    feat: print list using DmMenuDao
    DmMenuDao를 이용해서 list출력

--- spring-study/src/main/java/com/newlecture/spring/App.java
+++ spring-study/src/main/java/com/newlecture/spring/App.java
@@ -1,9 +1,17 @@
 package com.newlecture.spring;
 
+import java.util.List;
+
+import com.newlecture.spring.entity.Menu;
+import com.newlecture.spring.service.MenuServiceImp;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MenuServiceImp service = new MenuServiceImp();
+		List<Menu> list = service.getList();
+		
+		System.out.println(list);
 
 	}
 
--- spring-study/src/main/java/com/newlecture/spring/service/MenuServiceImp.java
+++ spring-study/src/main/java/com/newlecture/spring/service/MenuServiceImp.java
@@ -2,13 +2,20 @@ package com.newlecture.spring.service;
 
 import java.util.List;
 
+import com.newlecture.spring.dao.dm.DmMenuDao;
 import com.newlecture.spring.entity.Menu;
 
 public class MenuServiceImp implements MenuService{
-
+	
+	private DmMenuDao dao;
+	
+	public MenuServiceImp() {
+		dao = new DmMenuDao();
+	}
+	
 	@Override
 	public List<Menu> getList() {
 		// TODO Auto-generated method stub
-		return null;
+		return dao.findAll();
 	}
 }
