8929da79b4db2f91d300dc17e13b7cf57594c092
devhoody <rethnoly@gmail.com>
2023-09-14 10:14:03 +0900

    feat: AnnotationApplicationContext
    App.java
    ApplicationContext context = new AnnotationConfigApplicationContext(SpringConpig.class);
    이름을 이용한 경우
    App.java
    - MenuService service = (MenuService) context.getBean("service")
    @Component("service") - 이름
    이름을 이용하지 않은 경우
    App.java
    -  MenuService service = context.getBean(MenuService.class);
    @Component

--- spring-study/src/main/java/com/newlecture/spring/dao/dm/DmMenuDao.java
+++ spring-study/src/main/java/com/newlecture/spring/dao/dm/DmMenuDao.java
@@ -3,17 +3,20 @@ package com.newlecture.spring.dao.dm;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.newlecture.spring.dao.MenuDao;
 import com.newlecture.spring.entity.Menu;
 
+@Component
 public class DmMenuDao implements MenuDao {
 
 	@Override
 	public List<Menu> findAll() {
 		// TODO Auto-generated method stub
 		List<Menu> list = new ArrayList<Menu>();
-		list.add(new Menu(1L,"아메리카노","americano",3600,"coffee.png",null,0,1L));
-		list.add(new Menu(1L,"아이스 아메리카노","ice americano",4100,"coffee1.png",null,0,1L));
+		list.add(new Menu(1L, "아메리카노", "americano", 3600, "coffee.png", null, 0, 1L));
+		list.add(new Menu(1L, "아이스 아메리카노", "ice americano", 4100, "coffee1.png", null, 0, 1L));
 		return list;
 	}
 
--- spring-study/src/main/java/com/newlecture/spring/service/MenuServiceImp.java
+++ spring-study/src/main/java/com/newlecture/spring/service/MenuServiceImp.java
@@ -2,9 +2,14 @@ package com.newlecture.spring.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.newlecture.spring.dao.MenuDao;
 import com.newlecture.spring.entity.Menu;
 
+@Component
+//@Component("service") - 이름 명명
 public class MenuServiceImp implements MenuService {
 
 	private MenuDao dao; // 인터페이스를 쓰는 이유??
@@ -13,6 +18,7 @@ public class MenuServiceImp implements MenuService {
 //		dao = new DmMenuDao();
 	}
 
+	@Autowired // 자동으로 Dao와 연결해주는 Annotation
 	public void setDao(MenuDao dao) {
 		this.dao = dao;
 	}
