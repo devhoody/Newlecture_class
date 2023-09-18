0b382eed5421e450e3ac14238314a570db28e242
devhoody <rethnoly@gmail.com>
2023-09-18 11:53:15 +0900

    feat:  SpringBoot를 이용한 웹페이지 띄우기
    DB 연동을 빼고 웹페이지 띄우기 위해
    pom.xml에서 starter 관련 dependency를 모두 주석처리

---
+++ rlandweb/src/main/java/kr/co/rland/web/controller/HomeController.java
@@ -0,0 +1,16 @@
+package kr.co.rland.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@ResponseBody
+	@RequestMapping("index")
+	public String index() {
+		return "Hello Spring!!";
+	}
+}
