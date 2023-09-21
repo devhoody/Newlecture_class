c9af1773c948e46e562c30c4c610a5e740f8c3a3
devhoody <rethnoly@gmail.com>
2023-09-21 14:07:05 +0900

    Feat: thymeleaf반복문, service, repository(@Mapper, @Select) 추가
    th:each="m : ${list}" 추가
    application.properties
    - jdbc 서버 연결 설정
    
    - Lommok 라이브러리 추가

---
+++ rlandweb/src/main/java/kr/co/rland/web/entity/Menu.java
@@ -0,0 +1,16 @@
+package kr.co.rland.web.entity;
+
+import java.util.Date;
+
+public class Menu {
+		private long id;
+	   private String korName;
+	   private String engName;
+	   private int price;
+	   private String img;
+	   private Date regDate;
+	   private int hit;
+	   private long memberId;
+	   
+	   
+}
