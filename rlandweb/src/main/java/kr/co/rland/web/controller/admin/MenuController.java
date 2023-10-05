3a5f91939e4e37a73b40f3cd6394ae67ead708f0
devhoody <rethnoly@gmail.com>
2023-10-05 15:20:01 +0900

    Feat: admin list,reg 구현

--- rlandweb/src/main/java/kr/co/rland/web/controller/admin/MenuController.java
+++ rlandweb/src/main/java/kr/co/rland/web/controller/admin/MenuController.java
@@ -1,24 +1,84 @@
 package kr.co.rland.web.controller.admin;
 
+import kr.co.rland.web.entity.Menu;
+import kr.co.rland.web.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
 
 @Controller("adminMenuController")
 @RequestMapping("/admin/menu")
 public class MenuController {
 
-	@ResponseBody
+	@Autowired
+	private MenuService service;
+
+//	@ResponseBody
 	@RequestMapping("list")
 	public String list() {
 
-		return "- 관리자메뉴리스트";
+		return "/admin/menu/list";
 	}
 
-	@ResponseBody
+//	@ResponseBody
 	@RequestMapping("detail")
 	public String detail() {
 
-		return "관리자 세부사항";
+		return "admin/menu/detail";
+	}
+
+	@GetMapping("reg")
+	public String reg() {
+
+		return "admin/menu/reg";
+	}
+
+	@PostMapping ("reg")
+	public String reg(
+//			Menu menu
+			@RequestParam("img-name") MultipartFile imgFile,
+			@RequestParam("kor-name") String korName,
+			@RequestParam("eng-name") String engName,
+			@RequestParam(defaultValue = "0") int price,
+			String description,
+			@RequestParam("is-small") boolean isSmall
+	) throws IOException {
+
+		Menu menu = Menu.builder()
+					.korName(korName)
+					.engName(engName)
+					.price(price)
+					.isSmall(isSmall)
+					.description(description)
+					.img(imgFile.getOriginalFilename())
+					.memberId(1L)
+					.build();
+
+		service.add(menu);
+
+		System.out.println(imgFile.isEmpty());
+		System.out.println(imgFile.getOriginalFilename());
+
+		InputStream fis = imgFile.getInputStream();
+		OutputStream fos = new FileOutputStream("/Users/eddy/user/"+ imgFile.getOriginalFilename());
+
+		int size =0;
+		byte[] bfr = new byte[1024];
+		while((size=fis.read(bfr)) != -1)
+			fos.write(bfr, 0, size);
+
+		fis.close();
+		fos.close();
+
+//		System.out.println(engName);
+//		System.out.println(korName);
+//		System.out.println(price);
+//		System.out.println(menu);
+//		System.out.println(isSmall);
+		return "redirect:list";
 	}
 }
