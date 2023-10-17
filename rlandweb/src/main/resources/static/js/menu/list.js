0cf9a08b9e4df201d843081747a0c6c2a4231620
devhoody <rethnoly@gmail.com>
2023-10-17 10:11:26 +0900

    Feat: await async을 이용한 데이터 처리
    XHR => await, async를 이용해 response를 활용한다.

--- rlandweb/src/main/resources/static/js/menu/list.js
+++ rlandweb/src/main/resources/static/js/menu/list.js
@@ -2,19 +2,44 @@ window.addEventListener("load", function () {
     // let btnSubmit = document.querySelector(".form-section .btn-find");
     let findButton = document.querySelector(".query-filter .btn-find");
     let inputQuery = document.querySelector(".query-filter .input-query");
+
     let categoryFilterSection = document.querySelector(".category-filter");
     let categoryUl = categoryFilterSection.querySelector("ul");
     let currentCategory = categoryUl.querySelector("li:first-child a");
+    // let hearts = document.querySelectorAll(".menu-card-list .btn-like");
+    //
+    //
+    //
+    // for(let i=0; i<hearts.length; i++){
+    //     hearts[i].onclick = function(e){
+    //         e.preventDefault();
+    //         console.log("hello");
+    //     }
+    // }
 
     let content = document.querySelector(".menu-card-list .content");
 
-    function bind(text){
-        let list = JSON.parse(text);
+    content.onclick = (e)=>{
+        e.preventDefault();
+
+        let el = e.target;
+        if(!el.classList.contains("btn-like"))
+            return;
+
+        console.log("hello");
+        // `/api/menus?c=${el.dataset.id}`
+        // `/api/menus/likes`
+        `/api/menus?id=${id}&isLike=${islike}}`
+        // `/api/
+    }
+
+    function bind(json){
+        // let list = JSON.parse(text);
         // alert(request.responseText);
 
         content.innerHTML = "";
         // section을 위한 DOM 객체를 직접 생성해서 append 한다.
-        for (let m of list) {
+        for (let m of json) {
             let iconHeart = m.isLike ? "icon-heart-fill" : "icon-heart";
 
             let template = `
@@ -41,7 +66,8 @@ window.addEventListener("load", function () {
             // content.innerHTML = content.innerHTML + template; // 때려부수고 새로 짓는거
         }
     }
-    categoryUl.onclick = function(e){
+
+    categoryUl.onclick = async function(e){
         e.preventDefault();
 
         let el = e.target;
@@ -55,64 +81,39 @@ window.addEventListener("load", function () {
         el.classList.add("current");
         currentCategory = el;
 
-        console.log(el.href);
         console.log(el.dataset.id);
 
-        console.log("클릭했씀.");
-
         // 카테고리 id 전달
-        let request = new XMLHttpRequest();
-        request.open("GET", `/api/menus?c=${el.dataset.id}`, true);
-        request.onload = function(){
-            bind(request.responseText);
-        }
-        request.send();
+        let response = await fetch(`/api/menus?c=${el.dataset.id}`);
+        let list = await response.json();
+        bind(list);
+
+        // let request = new XMLHttpRequest();
+        // request.open("GET", `/api/menus?c=${el.dataset.id}`, true);
+        // request.onload = function(){
+        //     bind(request.responseText);
+        // }
+        // request.send();
     }
 
-    findButton.onclick = function (e) {
+    findButton.onclick = async function (e) {
         e.preventDefault();
 
         let query = inputQuery.value;
 
-        let request = new XMLHttpRequest();
-        request.open("GET", `/api/menus?q=${query}`, true); // true : 비동기
-        request.onload = function () {
-            let list = JSON.parse(request.responseText);
-            // alert(request.responseText);
+        let response = await fetch(`/api/menus?q=${query}`);
+        let list = await response.json();
+        bind(list);
 
-            content.innerHTML = "";
-            // section을 위한 DOM 객체를 직접 생성해서 append 한다.
-            for (let m of list) {
-                let iconHeart = m.isLike ? "icon-heart-fill" : "icon-heart";
-
-                let template = `
-                <section class="menu-card">
-                    <h1>
-                        <a class="heading-3" href="detail.html">${m.korName}</a></h1>
-                    <h2 class="heading-2 font-weight:normal color:base-5">${m.engName}</h2>
-                    <div class="price-block d:flex align-items:flex-end">
-                        <span class="font-weight:bold" >${m.price}</span>원<span class="soldout-msg ml:auto mr:auto md:d:none">SOLD OUT</span></div>
-                    <div class="img-block">
-                        <a href="detail?id=${m.id}"><img src="/image/menu/product/${m.img}"></a>
-                    </div>
-                    <div class="like-block d:flex justify-content:flex-end">
-                        <a  class="icon icon-color:base-4 ${iconHeart}" href="">좋아요</a>
-                        <span class="font-weight:bold ml:1">${m.likeCount}</span>
-                    </div>
-                    <div class="pay-block d:flex">
-                        <a class="icon md:icon:none icon-cart icon-color:base-0 color:base-0 btn-type:icon btn-cart md:btn-type:text" href="">담기</a>
-                        <a class="icon md:icon:none icon-card icon-color:base-0 color:base-0 btn-type:icon btn-card md:btn-type:text" href="">주문하기</a>
-                    </div>
-                </section>
-                `;
-                content.insertAdjacentHTML("beforeend", template); // 때려부수지 않고 새로 짓는거
-                // content.innerHTML = content.innerHTML + template; // 때려부수고 새로 짓는거
-            }
-        };
-        request.send();
+        // let request = new XMLHttpRequest();
+        // request.open("GET", `/api/menus?q=${query}`, true); // true : 비동기
+        // request.onload = function () {
+        //     bind(request.responseText);
+        // };
+        // request.send();
     };
 
-    });
+});
 
 
 
