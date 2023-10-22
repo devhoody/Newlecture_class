0a61c1eb1ef75230125c86557f2d29ed289513e8
devhoody <rethnoly@gmail.com>
2023-10-22 21:28:58 +0900

    feat: memberController 주석처리

--- rlandweb/src/main/java/kr/co/rland/web/controller/MemberController.java
+++ rlandweb/src/main/java/kr/co/rland/web/controller/MemberController.java
@@ -11,11 +11,11 @@ public class MemberController {
 
     @GetMapping("index")
     public String index(HttpSession session){
-        Object username_ = session.getAttribute("username");
-
-        // 내가 있던 곳 이동
-        if(username_ == null)
-            return "redirect:../user/login?returnURL=/member/index";
+//        Object username_ = session.getAttribute("username");
+//
+//        // 내가 있던 곳 이동
+//        if(username_ == null)
+//            return "redirect:../user/login?returnURL=/member/index";
 
         // 홈페이지 이동
         return "member/index";
