b685828d84e2436260a328f9f6929f0a1e00a8fc
devhoody <rethnoly@gmail.com>
2023-10-27 09:37:59 +0900

    feat: cors allowed method 추가

--- rlandweb/src/main/java/kr/co/rland/web/config/CorsConfig.java
+++ rlandweb/src/main/java/kr/co/rland/web/config/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry
                         .addMapping("/**")
-                            .allowedOrigins("http://127.0.0.1:5500/");
+                            .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedMethods("POST","PUT","DELETE","GET");
             }
         };
 
