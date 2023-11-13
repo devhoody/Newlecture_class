8b0de11c835d45b5ccc9f522b2b6a8db17f039c9
devhoody <rethnoly@gmail.com>
2023-11-13 16:56:45 +0900

    feat: 에러페이지 구현

---
+++ rlandweb/src/main/java/kr/co/rland/web/config/ControllerExceptionConfig.java
@@ -0,0 +1,16 @@
+package kr.co.rland.web.config;
+
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionConfig {
+
+    @ExceptionHandler(Exception.class)
+    public String error(Exception e, Model model){
+        model.addAttribute("msg","전역 에러 죄송합니다!");
+        return "error";
+    }
+}
